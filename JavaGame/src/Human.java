80e8daf5a7e8933875ff56345f74561df7a80dc6
smgzach <zm989392@gmail.com>
2025-02-17 23:06:10 -0500

    Update Human.java

--- JavaGame/src/Human.java
+++ JavaGame/src/Human.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class Human 
 {
+	int health;
+	int attackStat;
+	int defenseStat;
 	
+	public Human(int healthin, int attackin, int defensein)
+	{
+		health = healthin;
+		attackStat = attackin;
+		defenseStat = defensein;
+
+	}
 	
+
 	
 	
 }
