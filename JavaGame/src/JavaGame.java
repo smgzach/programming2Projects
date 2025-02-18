bffdde21f1850ab1670d8be0cf55023c70dc1c1a
smgzach <zm989392@gmail.com>
2025-02-17 23:05:35 -0500

    I LOVE JAVA

--- JavaGame/src/JavaGame.java
+++ JavaGame/src/JavaGame.java
@@ -1,9 +1,13 @@
-
+import java.util.Scanner;
 public class JavaGame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Scanner scn = new Scanner(System.in);
+		Human warrior = new Human(100, 25, 25);
+		Human archer = new Human(90, 45, 454);
+		System.out.println("initialize your human");
+		
 	}
 
 }
